fbd38a91e475e9a67cd5c228b7e1ad8467924170
Ola Olsen <olaolsen22@gmail.com>
2021-10-23 13:12:30 +0800

    Added display-devices component, verification as well if user is logged in before entering /devices page

--- src/pages/devices/devices.jsx
+++ src/pages/devices/devices.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import { useState, Component } from "react";
 import './devices.scss'
+// COMPONENTS
+import { DisplayDevices } from '../../components/display-devices/display-devices'
+import axios from "axios";
 
-export const Devices = () => {
-    return (
-        <div>
-            <h1>Devices</h1>
-        </div>
-    )
+class Devices extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            deviceList: []
+        }
+    }
+    componentDidMount() {
+        this.getDevices();
+        setInterval(this.getDevices, 5000);
+    }
+    getDevices = () => {
+        axios.get('http://35.201.2.209:8000/devices').then(response => {
+            this.setState({deviceList: response.data.devices})
+        })
+    }
+    render () {
+        return (
+            <div className='devices-container'>
+                <DisplayDevices deviceList={this.state.deviceList}/>
+            </div>
+        )
+    }
 }
+
+export default Devices;
